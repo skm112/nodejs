e7b72f868afafef19c868a2580d3372c2bda4967
skm112 <shekharsandhole@gmail.com>
2018-09-28 11:59:47 +0530

    api update and delete

--- server/api/country.api.js
+++ server/api/country.api.js
@@ -19,21 +19,42 @@ router.post('/save',(req,res)=>{
         name:req.body.name,
         st_date: new Date()
     };
+    var newRecord = new country(data);
+    newRecord.save((err, docs) => {
+        if (err) throw err;
+        res.send({ 'msg': 'success' });
+    });
 
-//route--Delete------------------
-
-router.delete('/delete',(req,res)=>{
     
 })
 
-//--------------------------------------
-var newRecord=new country(data);
-newRecord.save((err,docs)=>{
-    if(err)throw err;
-    res.send({'msg':'success'});
-});
+//route--Delete------------------
 
+router.delete('/delete/:id',(req,res)=>{
+    country.findByIdAndRemove(mongojs.ObjectId(req.params.id),(err)=>{
+        if(err) res.send(err);
+        res.send({msg:'success'});
+    })
+    
 })
+    //route--PUT--for-update------
+
+    router.put('/update/:id', (req, res) => {
+        console.log("update");
+        var _id = req.params.id;
+        console.log(_id);
+        var query = { '_id': mongojs.ObjectId(_id) };
+        var update = { name: req.body.name };
+        country.findOneAndUpdate(query, update, (err, doc) => {
+            if (err) throw err;
+            res.json(doc);
+        })
+    });
+
+//--------------------------------------
+
+
+
 
 
 module.exports=router;
