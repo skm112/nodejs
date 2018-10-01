27dfc80c18ce1187e89fb71ea4ebe7477040ac50
skm112 <shekharsandhole@gmail.com>
2018-10-01 13:43:41 +0530

    paging done

--- server/api/country.api.js
+++ server/api/country.api.js
@@ -1,13 +1,14 @@
 var express=require('express');
 var router=express.Router();
 var mongojs = require("mongojs");
+var mongoose=require("mongoose");
 var country=require('../models/country.js');
 
 //route--get----
 router.get('/list',(req,res)=>{
 country.find({},(err,docs)=>{
     if (err) throw err;
-    res.end(JSON.stringify(docs));
+    res.end(JSON.stringify(docs.length));
 })
 });
 
@@ -15,7 +16,8 @@ country.find({},(err,docs)=>{
 
 router.post('/save',(req,res)=>{
     var data={
-        _id: mongojs.ObjectId(),
+        _id:mongoose.Types.ObjectId(),
+       // _id: mongojs.ObjectId(),
         name:req.body.name,
         st_date: new Date()
     };
@@ -31,7 +33,7 @@ router.post('/save',(req,res)=>{
 //route--Delete------------------
 
 router.delete('/delete/:id',(req,res)=>{
-    country.findByIdAndRemove(mongojs.ObjectId(req.params.id),(err)=>{
+    country.findByIdAndRemove(mongoose.Types.ObjectId(req.params.id),(err)=>{
         if(err) res.send(err);
         res.send({msg:'success'});
     })
@@ -43,13 +45,29 @@ router.delete('/delete/:id',(req,res)=>{
         console.log("update");
         var _id = req.params.id;
         console.log(_id);
-        var query = { '_id': mongojs.ObjectId(_id) };
+        var query = { '_id': mongoose.Types.ObjectId(_id) };
         var update = { name: req.body.name };
         country.findOneAndUpdate(query, update, (err, doc) => {
             if (err) throw err;
             res.json(doc);
         })
     });
+//route--POST --for--paging
+    router.post('/pageno/list',(req,res)=>{
+        var limit=req.body.limit;
+        var pageNo=req.body.pageno;
+        var skip=(pageNo-1)*limit;
+        console.log('skip > ' + skip);
+        var cursor=country.aggregate([
+                {$skip:skip},
+                {$limit:limit},
+        ]);
+        
+        cursor.exec((err,docs)=>{
+            console.log(docs);
+            res.end(JSON.stringify(docs));
+        })
+    })
 
 //--------------------------------------
 
